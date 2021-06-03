f8e12c2e91b2ebf917c24c87b39b02783af60f6d
zeesyed <haizafrul@gmail.com>
2021-06-03 10:20:39 -0500

    Update java_working_csv_insert.java

--- java_working_csv_insert.java
+++ java_working_csv_insert.java
@@ -2,6 +2,7 @@ package com.cassandra.pilot;
 
 import com.datastax.driver.core.*;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
 import java.io.BufferedReader;
@@ -13,13 +14,14 @@ public class ValidateCluster {
     public static void main(String[] args) {
         Cluster validateCluster = null;
 
-        String csvFile = "C:\\Users\\zsyed\\Downloads\\flights\\flights_from_pg.csv"; // Parse
+        String csvFile = "C:\\Users\\zsyed\\Downloads\\flights\\flights_from_pg_1.csv"; // Parse
         String line = ""; // Parse
         String cvsSplitBy = ","; // Parse
-
+        String str2 = "2012/11/11";
+        System.out.println(getdate1_fl_date(str2));
         try {
             validateCluster = validateCluster.builder() // (1)
-                    .addContactPoint("10.2.2.109")
+                    .addContactPoint("10.2.2.22")
                     // .withPort(9160)
                     .build();
             Session session = validateCluster.connect("testflight01"); // (2)
@@ -29,23 +31,21 @@ public class ValidateCluster {
 
             System.out.println(row.getString("release_version")); // (4)
 
+            PreparedStatement statement = session.prepare(
+
+                    "INSERT INTO flights"
+                            + "(ID , YEAR ,DAY_OF_MONTH , FL_DATE , AIRLINE_ID , CARRIER , FL_NUM , ORIGIN_AIRPORT_ID , ORIGIN , ORIGIN_CITY_NAME , ORIGIN_STATE_ABR , DEST , DEST_CITY_NAME , DEST_STATE_ABR , DEP_TIME , ARR_TIME , ACTUAL_ELAPSED_TIME , AIR_TIME , DISTANCE)"
+                            + "VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);");
+
             // date1_fl_date
-            String sDate1 = "2012/11/11";
-            SimpleDateFormat formatter1 = new SimpleDateFormat("yyyy/MM/dd");
-            formatter1.setTimeZone(TimeZone.getTimeZone("UTC"));
-            Date date1_fl_date = formatter1.parse(sDate1);
+            // String sDate1 = "2012/11/11";
+            // Date date1 = date1_fl_date(sDate1);
             // date3_dep_arr_date
-            String sDate3 = "1234";
-            SimpleDateFormat formatter3 = new SimpleDateFormat("yyyy/MM/ddHHmm");
-            formatter3.setTimeZone(TimeZone.getTimeZone("UTC"));
-            Date date3_dep_arr_date = formatter3.parse(sDate1 + sDate3);
-            System.out.println(date3_dep_arr_date);
+            // String sDate3 = "1234";
+            // Date date3 = date3_dep_arr_date(sDate3);
             // date5_actl_air_date
-            int sDate5 = 124;
-            String sDateConvert4 = minutestoHHmm(sDate5);
-            SimpleDateFormat formatter5 = new SimpleDateFormat("yyyy/MM/ddHHmm");
-            formatter5.setTimeZone(TimeZone.getTimeZone("UTC"));
-            Date date5_actl_air_date = formatter5.parse(sDate1 + sDateConvert4);
+            // int sDate5 = 124;
+            // Date date5 = date5_actl_air_date(sDate5);
 
             try {
                 BufferedReader br = new BufferedReader(new FileReader(csvFile));
@@ -72,17 +72,16 @@ public class ValidateCluster {
                     String ORIGIN = flights[8];
                     String ORIGIN_CITY_NAME = flights[9];
                     String ORIGIN_STATE_ABR = flights[10];
-
-                    PreparedStatement statement = session.prepare(
-
-                            "INSERT INTO flights"
-                                    + "(ID , YEAR ,DAY_OF_MONTH , FL_DATE , AIRLINE_ID , CARRIER , FL_NUM , ORIGIN_AIRPORT_ID , ORIGIN , ORIGIN_CITY_NAME , ORIGIN_STATE_ABR , DEST , DEST_CITY_NAME , DEST_STATE_ABR , DEP_TIME , ARR_TIME , ACTUAL_ELAPSED_TIME , AIR_TIME , DISTANCE)"
-                                    + "VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);");
+                    Date FL_DATE = date1_fl_date(flights[3]);
+                    Date DEP_TIME = date3_dep_arr_date(flights[14], flights[3]);
+                    Date ARR_TIME = date3_dep_arr_date(flights[15], flights[3]);
+                    Date ACTUAL_ELAPSED_TIME = date5_actl_air_date(Integer.valueOf(flights[16]).intValue(), flights[3]);
+                    Date AIR_TIME = date5_actl_air_date(Integer.valueOf(flights[17]).intValue(), flights[3]);
 
                     BoundStatement boundStatement = new BoundStatement(statement);
-                    session.execute(boundStatement.bind(ID, YEAR, DAY_OF_MONTH, date1_fl_date, AIRLINE_ID, CARRIER,
-                            FL_NUM, ORIGIN_AIRPORT_ID, ORIGIN, ORIGIN_CITY_NAME, ORIGIN_STATE_ABR, DEST, DEST_CITY_NAME,
-                            DEST_STATE_ABR, new Date(), new Date(), new Date(), new Date(), DISTANCE));
+                    session.execute(boundStatement.bind(ID, YEAR, DAY_OF_MONTH, FL_DATE, AIRLINE_ID, CARRIER, FL_NUM,
+                            ORIGIN_AIRPORT_ID, ORIGIN, ORIGIN_CITY_NAME, ORIGIN_STATE_ABR, DEST, DEST_CITY_NAME,
+                            DEST_STATE_ABR, DEP_TIME, ARR_TIME, ACTUAL_ELAPSED_TIME, AIR_TIME, DISTANCE));
                 }
 
             } catch (IOException e) {
@@ -98,10 +97,75 @@ public class ValidateCluster {
     }
 
     private static String minutestoHHmm(int t) {
-        int hours = t / 60; // since both are ints, you get an int
-        int minutes = t % 60;
-        String hrs = String.format("%02d", hours);
-        String mins = String.format("%02d", minutes);
-        return String.valueOf(hrs) + String.valueOf(mins);
+        String hrs = null;
+        String mins = null;
+        try {
+            int hours = t / 60; // since both are ints, you get an int
+            int minutes = t % 60;
+            hrs = String.format("%02d", hours);
+            mins = String.format("%02d", minutes);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            return String.valueOf(hrs) + String.valueOf(mins);
+        }
     }
+
+    private static Date getdate1_fl_date(String sDate1) {
+        Date date1_fl_date = null;
+        try {
+            SimpleDateFormat formatter1 = new SimpleDateFormat("yyyy/MM/dd");
+            formatter1.setTimeZone(TimeZone.getTimeZone("UTC"));
+            date1_fl_date = formatter1.parse(sDate1);
+
+        } catch (ParseException e) {
+            // e.printStackTrace();
+            // continue;
+        }
+        return date1_fl_date;
+
+    }
+
+    private static Date date1_fl_date(String sDate1) {
+        int excep1 = 0;
+        SimpleDateFormat formatter1 = new SimpleDateFormat("yyyy/MM/dd");
+        Date date1_fl_date = null;
+        try {
+            formatter1.setTimeZone(TimeZone.getTimeZone("UTC"));
+            date1_fl_date = formatter1.parse(sDate1);
+        } catch (ParseException e) {
+            // e.printStackTrace();
+            excep1++;
+        }
+        // System.out.println("Total ");
+        // System.out.println(date1_fl_date);
+        return date1_fl_date;
+
+    }
+
+    private static Date date3_dep_arr_date(String sDate3, String sDate1) {
+        SimpleDateFormat formatter3 = new SimpleDateFormat("yyyy/MM/ddHHmm");
+        Date date3_dep_arr_date = null;
+        try {
+            formatter3.setTimeZone(TimeZone.getTimeZone("UTC"));
+            date3_dep_arr_date = formatter3.parse(sDate1 + sDate3);
+        } catch (ParseException e) {
+            // e.printStackTrace();
+            int excep2 = 0;
+            excep2++;
+        }
+        // System.out.println(date3_dep_arr_date);
+        return date3_dep_arr_date;
+    }
+
+    private static Date date5_actl_air_date(int sDate5, String sDate1) throws ParseException {
+        String sDateConvert4 = minutestoHHmm(sDate5);
+        SimpleDateFormat formatter5 = new SimpleDateFormat("yyyy/MM/ddHHmm");
+        formatter5.setTimeZone(TimeZone.getTimeZone("UTC"));
+        Date date5_actl_air_date = formatter5.parse(sDate1 + sDateConvert4);
+        // System.out.println(date5_actl_air_date);
+        return date5_actl_air_date;
+    }
+
 }
